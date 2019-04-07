5910d17c842f0f4482161046dbaa11515637fb52
LeeYY26088 <26088@naver.com>
2019-04-07 20:47:23 +0900

    폴더 이름 변경

---
+++ algorithm_lecture/lec03_3/algorithm_lecture/Person2.java
@@ -0,0 +1,30 @@
+package algorithm_lecture;
+
+public class Person2 {
+	String name;
+	String sex;
+	String bloodType;
+
+	public Person2(String name, String sex, String bloodType) {
+		this.name = name;
+		this.sex = sex;
+		this.bloodType = bloodType;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public String getBloodType() {
+		return bloodType;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Person{%s, %s, %s}", name, sex, bloodType);
+	}
+}
